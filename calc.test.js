e28410d65804c887675ccb30c6cb3f32270c517c
Witni <witni.whittle@icloud.com>
2020-08-10 18:50:24 -0500

    Final Commit, Completed Jest project

--- calc.test.js
+++ calc.test.js
@@ -7,40 +7,48 @@ import path from 'path';
 const html = fs.readFileSync(path.resolve(__dirname, './index.html'), 'utf8');
 
 let dom;
-let container;
+let form;
 
-describe('index', () => {
+describe('calculator', () => {
   beforeEach(() => {
     // Constructing a new JSDOM with this option is the key
     // to getting the code in the script tag to execute.
     // This is indeed dangerous and should only be done with trusted content.
     // https://github.com/jsdom/jsdom#executing-scripts
     dom = new JSDOM(html, { runScripts: 'dangerously' });
-    container = dom.window.document.body;
+    form = dom.window.document.body;
   })
 
   test('has an add button', () => {
-    expect(container.querySelector('button')).not.toBeNull();
-    expect(getByText(container, '+')).toBeInTheDocument();
+    expect(form.querySelector('#add')).not.toBeNull();
+    expect(getByText(form, '+')).toBeInTheDocument();
   })
-
+  test('has a multiply button', () => {
+    expect(form.querySelector('#multiply')).not.toBeNull();
+    expect(getByText(form, '*')).toBeInTheDocument();
+  })
+  test('has a divide button', () => {
+    expect(form.querySelector('#divide')).not.toBeNull();
+    expect(getByText(form, '/')).toBeInTheDocument();
+  })
   test('has a subtract button', () => {
-    expect(container.querySelector('subtractBy')).not.toBeNull();
-    expect(getByText(container, '-')).toBeInTheDocument();
+    expect(form.querySelector('#subtract')).not.toBeNull();
+    expect(getByText(form, '-')).toBeInTheDocument();
   })
 
   test('can add two number', () => {
 
-    let value = container.querySelector("#firstNumber");
-    let value = container.querySelector("#secondNumber");
+    let firstNumber = form.querySelector("#firstNumber");
+    let secondNumber = form.querySelector("#secondNumber");
 
-    fireEvent.change(number1, {target: { value: 3 } });
-    fireEvent.change(number2, {target: { value: 2 } });
+    fireEvent.change(firstNumber, {target: { value: 3 } });
+    fireEvent.change(secondNumber, {target: { value: 2 } });
 
-    const button = getByText(container, '+');
+    const button = getByText(form, '+');
     fireEvent.click(button);
 
-    let result = container.querySelector("#output");
+    let result = form.querySelector("#output");
+    console.log(result);
     expect(parseFloat(result.innerHTML)).toBe(5);
   })
 
